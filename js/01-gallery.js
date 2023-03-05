cafa2bd3030b3a192aa0c9877e909008707aba45
Dinis PC <DinisZabrodsky@gmail.com>
2023-03-05 21:18:05 +0200

    домашня робота

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,42 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+
+gallery.insertAdjacentHTML("beforeend", createGalery(galleryItems));
+gallery.addEventListener('click', openImage);
+
+
+function createGalery(gallery) {
+    return gallery.map(({ preview, original, description }) => {
+        return`
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}" rel="noopener noreferrer">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </div>`
+    }).join("");
+
+}
+
+
+
+function openImage(event) {
+    event.preventDefault();
+    const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="1280" height="855">`);
+
+    instance.show()
+
+}
+
+function cloceImageOnEsc() {
+    
+};
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+gallery.insertAdjacentHTML("beforeend", createGalery(galleryItems));
+// gallery.addEventListener('click', openImage);
+
+
+function createGalery(gallery) {
+    return gallery.map(({ preview, original, description }) => {
+        return `
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`
+    }).join("");
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250
+});
